76d96cd94ebc9ddd867a64b89c60f7a41d0234de
Tacobil <Tacobil123@gmail.com>
2026-04-27 00:38:10 +0200

    script organization

--- js/feed.js
+++ js/feed.js
@@ -1,13 +1,3 @@
-const sidebar = document.querySelector(".sidebar")
-const menuButton = document.querySelector(".menu-button")
-const fadeLayer = document.querySelector(".fade-layer")
-
-function toggleSidebar() {
-  sidebar.classList.toggle("toggle-sidebar")
-  fadeLayer.classList.toggle("show")
-}
-fadeLayer.addEventListener("click", toggleSidebar)
-menuButton.addEventListener("click", toggleSidebar)
 
 const users = {
   christianBuehner: {
