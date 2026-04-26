530819f94fa8b1e1e7a60d7135572e0b183be9e0
Tacobil <Tacobil123@gmail.com>
2026-04-26 10:17:26 +0200

    index complete

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 var sidebar = document.querySelector(".sidebar")
 var menuButton = document.querySelector(".menu-button")
-
+var fadeLayer = document.querySelector(".fade-layer")
 
 function toggleSidebar() {
-  sidebar.classList.toggle("small-sidebar")
+  sidebar.classList.toggle("toggle-sidebar")
+  fadeLayer.classList.toggle("show")
 }
-
+fadeLayer.addEventListener("click", toggleSidebar)
 menuButton.addEventListener("click", toggleSidebar)
